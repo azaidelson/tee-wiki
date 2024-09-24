5c17ba0759d79b5521b0ffc15598aa81e3bd90c1
exh0115 <165619875+exh0115@users.noreply.github.com>
2024-09-24 14:53:03 +0800

    Fix docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://tee-wiki.github.io/tee-wiki',
+  url: 'https://tee-wiki.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/tee-wiki',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
