116decfd77ea4d6fb44a7b764d1ec6f65dfb061c
Te Ye <6906654+teye@users.noreply.github.com>
2025-06-23 18:05:59 +0800

    fix baseUrl in different env

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://tee-wiki.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/tee-wiki/',
+  baseUrl: `${process.env.NODE_ENV === 'production' ? 'tee-wiki' : '/'}`,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
